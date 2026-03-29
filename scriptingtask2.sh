51e3453b0c1edda1e9c70349bfc5065054a118fa
aligoldstraw <alasdair.goldstraw@gmail.com>
2026-03-29 10:13:51 -0400

    Stage 1, Configuration data file creation

---
+++ scriptingtask2.sh
@@ -0,0 +1,9 @@
+!/bin/bash
+
+# HPC Job Scheduler Script for University High Performance Computing Laboratory
+# Developed with Bash
+
+# Configuration - data files (these are created automatically if they are missing)
+QUEUE_FILE="job_queue.txt"
+COMPLETED_FILE="completed_jobs.txt"
+LOG_FILE="scheduler_log.txt"
